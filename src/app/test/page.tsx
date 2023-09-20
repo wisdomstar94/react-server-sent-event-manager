7a8a4095d86d7cd910306cfad70e2548d1255dbc
wisdomstar94 <wisdomstar94@gmail.com>
2023-09-20 12:21:24 +0900

    v0.0.5 : npm 배포 셋팅.

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,14 @@
+"use client"
 import { useServerSentEventManager } from "@/hooks/use-server-sent-event-manager/use-server-sent-event-manager.hook";
+import { useEffect } from "react";
 
 export default function Page() {
   const serverSentEventManager = useServerSentEventManager();
+
+  useEffect(() => {
+    console.log(serverSentEventManager.isConnected('...'));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div>
